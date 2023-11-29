375a8196458b1860f1cbb302d7c603ea7fac0aab
TrymCoder <trymel.koder@gmail.com>
2023-11-29 09:12:46 +0100

    commit

--- imgbutton.js
+++ imgbutton.js
@@ -1,8 +1,18 @@
-var currentImage = 1;
-var images = ['/image/hogwarts 33.jpeg', '/image/Hogwarts day.jpeg', '/image/Hogwarts Night.png', '/image/hogwarts44.webp', '/image/Hogwarts day.jpeg'];
-
-function changeBackground() {
-    var backgroundContainer = document.getElementById('background-container');
-    currentImage = (currentImage % images.length) + 1;
-    backgroundContainer.style.backgroundImage = "url('" + images[currentImage - 1] + "')";
-}
+function toggleImageSize(image) {
+    if (image.classList.contains('fullscreen')) {
+      // If the image is already fullscreen, revert to normal size
+      image.classList.remove('fullscreen');
+    } else {
+      // If the image is not fullscreen, make it fullscreen
+      image.classList.add('fullscreen');
+    }
+  
+    // Reset the size of the other images
+    const images = document.querySelectorAll('.resizable-image');
+    images.forEach((img) => {
+      if (img !== image) {
+        img.classList.remove('fullscreen');
+      }
+    });
+  }
+  
